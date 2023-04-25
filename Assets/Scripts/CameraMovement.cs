e75169c0ff962393abce5159f658538d1c26ad21
FilGardFoil <a.ponikarovskii@gmail.com>
2023-04-25 10:17:59 +0300

    Like Zelda test studing project/

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Последнее обновление при построение кадра.
+    void LateUpdate()
+    {
+        // Определение движение камеры относительно цели с укзанной скоростью следования.
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z); // указание расположения камеры с учётом отдаления по оси Z.
+
+            // ограничение движение камеры по указанным позициям возможного минимального и максимального значения.
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+            
+
+        }
+    }
+}
---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour
+{
+    public bool playerInRange;
+    public string dialog;
+    public GameObject dialogBox;
+    public Text dialogText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Отображать дилог если игрок находиться в поле действия и нажал на пробел.
+        if (Input.GetButtonDown("action") && playerInRange)
+        {
+            if (dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+            }
+            else
+            {
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
+    }
+
+    // определение положения игрока в зоне действия триггера.
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogBox.SetActive(false);
+        }
+    }
+}
