34baaebb17a887d08c52f67ae1d4dbd3e559abc4
FilGardFoil <a.ponikarovskii@gmail.com>
2023-10-23 00:12:42 +0300

    1. Добавлена работа с сундуки и получением items
    2. Изменено назначение клавиш на геймпаде, действие с Y теперь на X
    3. Добавлена работа с анимацией полуения item для персонажа.
    4. добавлена анимация открывания сундука.

---
+++ Assets/Scripts/Objects/TreasureChest.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreasureChest : Interactable
+{
+    public Item contents;
+    public Inventory playerInventory;
+    public bool isOpen;
+    public SignaL raiseItem;
+    public GameObject dialogBox;
+    public Text dialogText;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("action") && playerInRange)
+        {
+            if (!isOpen)
+            {
+                // Открыть сундук.
+                OpenChest();
+            }
+            else
+            {
+                // Сундук уже открыт.
+                ChestAlreadyOpen();
+            }
+        }
+    }
+
+    public void OpenChest()
+    {
+        // Обработка событий при открытии сундука.        
+        dialogBox.SetActive(true);
+        dialogText.text = contents.itemDescription;
+        playerInventory.AddItem(contents);
+        playerInventory.currentItem = contents;
+        raiseItem.Raise();        
+        context.Raise();
+        isOpen = true;
+        animator.SetBool("opened", true);              
+    }
+
+    public void ChestAlreadyOpen()
+    {
+            dialogBox.SetActive(false);
+            raiseItem.Raise();
+    }
+
+    // определение положения игрока в зоне действия триггера.
+    public override void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger && !isOpen)
+        {
+            context.Raise();
+            playerInRange = true;
+        }
+    }
+
+    public override void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger && !isOpen)
+        {
+            context.Raise();
+            playerInRange = false;
+        }
+    }
+}
--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -21,6 +21,8 @@ public class PlayerMovement : MonoBehaviour
     public FloatValue currentHealth;
     public SignaL playerHealthSignal;
     public VectorValue startingPosition;
+    public Inventory playerInventory;
+    public SpriteRenderer receivedItemSprite;
 
     
     // Start is called before the first frame update
@@ -37,6 +39,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Обездвиживать игрока при взаимдействии с интерактивными объетами.
+        if (currentState == PlayerState.interact)
+            return;
+
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
@@ -60,7 +66,30 @@ public class PlayerMovement : MonoBehaviour
         yield return null;
         animator.SetBool("attacking", false);
         yield return new WaitForSeconds(.3f); // устанавливается время на ожидание выполнение анимации атаки.
-        currentState = PlayerState.walk;
+        
+        // Двигаться только если нет взаимодействий с интерактивными объектами.
+        if (currentState != PlayerState.interact)
+            currentState = PlayerState.walk;
+    }
+
+    public void RaiseItem()
+    {
+        if (playerInventory.currentItem != null)
+        {
+            if (currentState != PlayerState.interact)
+            {
+                animator.SetBool("receiveItem", true);
+                currentState = PlayerState.interact;
+                receivedItemSprite.sprite = playerInventory.currentItem.itemSprite; 
+            }
+            else
+            {
+                animator.SetBool("receiveItem", false);
+                currentState = PlayerState.idle;
+                receivedItemSprite.sprite = null;
+                playerInventory.currentItem = null;
+            }
+        }
     }
 
     // Реализация движения персонажа через обращение к аниматору, анимация движений сформирована через Bleeding trees.
