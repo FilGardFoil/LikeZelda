fbf068951989cdad3b6a133a08b2e69b5282e0e7
FilGardFoil <a.ponikarovskii@gmail.com>
2023-10-23 23:53:46 +0300

    Добавлен новый вид врага, Patrol Log.

---
+++ Assets/Scripts/Enemy Scripts/Log.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : Enemy
+{
+    public Rigidbody2D myRigidbody;
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform homePosition;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = EnemyState.idle;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        target = GameObject.FindWithTag("Player").transform;
+        animator.SetBool("wakeUp", true);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       CheckDistance(); 
+    }
+
+    public virtual void CheckDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= chaseRadius
+            && Vector3.Distance(target.position, transform.position) > attackRadius
+            && (currentState == EnemyState.idle || currentState == EnemyState.walk)
+            && currentState != EnemyState.stagger)
+        {
+            Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime); // движение до точки с которой может произвести атаку по цели.
+            ChangeAnimation(temp - transform.position);
+            myRigidbody.MovePosition(temp);            
+            ChangeState(EnemyState.walk);
+            animator.SetBool("wakeUp", true);
+        }
+        else if (Vector3.Distance(target.position, transform.position) > chaseRadius)
+        {
+            animator.SetBool("wakeUp", false);
+        }
+    }
+
+    private void SetAnimFloat(Vector2 setVector)
+    {
+        animator.SetFloat("moveX", setVector.x);
+        animator.SetFloat("moveY", setVector.y);
+    }
+
+    public void ChangeAnimation(Vector2 direction)
+    {
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            if (direction.x > 0)
+                SetAnimFloat(Vector2.right);
+            else if (direction.x < 0)
+                SetAnimFloat(Vector2.left);
+        }
+        else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+        {
+            if (direction.y > 0)
+                SetAnimFloat(Vector2.up);
+            else if (direction.y < 0)
+                SetAnimFloat(Vector2.down);
+        }
+    }
+
+    private void ChangeState(EnemyState newState)
+    {
+        if (currentState != newState)
+            currentState = newState;
+    }
+}
